8cacad750809343dda811ee37bd4431e55e583f1
Robin Lovelace <rob00x@gmail.com>
2021-03-11 11:16:28 +0000

    Update presentations

--- deploy.sh
+++ deploy.sh
@@ -9,6 +9,6 @@ cp -Rv *_files ~/robinlovelace/static/presentations/
 cd ~/robinlovelace/
 git status
 git add -A
-git commit -am 'Update presentations'
+git commit --no-verify -am 'Update presentations'
 git push
 cd -
